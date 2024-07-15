919a5739071c3929f54ef907de0dcb0924a20aa6
Nikita12G <gulyi08@mail.ru>
2024-07-15 14:12:13 +0700

    Initial Commit

---
+++ AuthScreen/AuthScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthScreenApp.swift
+//  AuthScreen
+//
+//  Created by Никита Гуляев on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
